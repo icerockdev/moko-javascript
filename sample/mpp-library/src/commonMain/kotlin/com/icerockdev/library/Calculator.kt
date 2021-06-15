a1af6fdd56a1d18fc53bfcb35b25a1baf067b272
Andrey Dorofeev <adorofeev@icerockdev.com>
2021-06-15 15:33:02 +0700

    added ios implementation for JavaScriptEngine

--- sample/mpp-library/src/commonMain/kotlin/com/icerockdev/library/Calculator.kt
+++ sample/mpp-library/src/commonMain/kotlin/com/icerockdev/library/Calculator.kt
@@ -4,10 +4,32 @@
 
 package com.icerockdev.library
 
-import dev.icerock.moko.javascript.multiply
+import dev.icerock.moko.javascript.JavaScriptEngine
+import dev.icerock.moko.javascript.JsType
 
 class Calculator {
-    fun run() {
-        println(multiply(a = 2, b = 3))
+    fun run(a: String, b: String): JsType {
+        val engine = JavaScriptEngine()
+        val testScript = "a+b"
+
+        val intA = a.toIntOrNull()
+        val intB = b.toIntOrNull()
+
+        val context = if (intA != null && intB != null) {
+            mapOf(
+                "a" to JsType.IntNum(intA),
+                "b" to JsType.IntNum(intB)
+            )
+        } else {
+            mapOf(
+                "a" to JsType.Str(a),
+                "b" to JsType.Str(b)
+            )
+        }
+
+        return engine.evaluate(
+            context = context,
+            script = testScript
+        )
     }
 }
